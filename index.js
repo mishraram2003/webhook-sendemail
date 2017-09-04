ae5831606dfe872bffcc9ac83b961048d68d355f
reachviji <reachvijiarun@gmail.com>
2017-09-04 14:43:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,28 +16,27 @@ restService.post('/sendmail', function(req, res) {
     var date = req.body.result && req.body.result.parameters && req.body.result.parameters.date ? req.body.result.parameters.date : "When"
     var content = req.body.result && req.body.result.parameters && req.body.result.parameters.Content ? req.body.result.parameters.Content : "What should the content of the mail be"
     var returntext = "Mail has been sent to" +mailto + "with subject" +subject + "on" +date + "and content is" +content
-	 var data = {
-    'subject': subject,
-    'to': mailto,
-    'body': content
-	}
+    var data = {
+        'subject': subject,
+        'to': mailto,
+        'body': content
+    }
    alert("hi");
     $.ajax({
-                url: 'http://localhost:8080/sendmail/outlook', // url where to submit the requesthttp://localhost:8086/RESTfulExample/rest/sendemail/outlook
-                type : "POST", // type of action POST || GET
-                dataType : 'json', // data type
-                data: JSON.stringify(data),
-               contentType: "application/json"	,			// post data || get data
-                success : function(result) {
-                    // you can see the result from the console
-                    // tab of the developer tools
-                    console.log(result);
-                },
-                error: function(xhr, resp, text) {
-                    console.log(xhr, resp, text);
-                }
-            })
-        });
+            url: 'http://localhost:8080/sendmail/outlook', // url where to submit the requesthttp://localhost:8086/RESTfulExample/rest/sendemail/outlook
+            type : "POST", // type of action POST || GET
+            dataType : 'json', // data type
+            data: JSON.stringify(data),
+            contentType: "application/json"	,			// post data || get data
+            success : function(result) {
+               // you can see the result from the console
+               // tab of the developer tools
+                console.log(result);
+            },
+            error: function(xhr, resp, text) {
+               console.log(xhr, resp, text);
+            }
+          });
     return res.json({
         speech: returntext,
         displayText: mailto + subject + date + content,
